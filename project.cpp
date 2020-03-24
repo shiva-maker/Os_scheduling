14e2548b61c6065795b63adfb88474b9d63db157
shiva-maker <62153877+shiva-maker@users.noreply.github.com>
2020-03-24 14:30:18 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -9,16 +9,3 @@ bool btimeSort(node a,node b){
     return a.btime < b.btime; 
 }
 bool btimeOppSort(node a,node b){
-    if(a.btime!=b.btime)
-        return a.btime > b.btime; 
-    return a.atime < b.atime;
-}
-int main(){
-    int nop,choice,i,qt;
-    cout<<"Enter number of processes\n";
-    cin>>nop;
-    cout<<"Enter process, AT, BT\n";
-    insert(nop);
-    disp(nop,1);
-    return 0;
-}
