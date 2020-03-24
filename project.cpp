c39e00e38873c3f6e49af39a5e9c731d3ad96448
shiva-maker <62153877+shiva-maker@users.noreply.github.com>
2020-03-24 14:30:27 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -2,8 +2,7 @@
         cin>>a[i].pname;
         cin>>a[i].atime;
         cin>>a[i].btime;
-        a[i].wtime=-a[i].atime+1;
-    }
+
 }
 bool btimeSort(node a,node b){
     return a.btime < b.btime; 
