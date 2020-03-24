2c646d2ef4ed67b26d172d1a67d243471cc14459
shiva-maker <62153877+shiva-maker@users.noreply.github.com>
2020-03-24 14:30:56 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,13 +1,3 @@
-
-        cin>>a[i].pname;
-        cin>>a[i].atime;
-        cin>>a[i].btime;
-
-}
-bool btimeSort(node a,node b){
-    return a.btime < b.btime; 
-}
-bool btimeOppSort(node a,node b){
 #include<iostream>
 #include<algorithm>
 using namespace std;
